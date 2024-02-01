6ac8e2647a7f6955426f734203a74520ce08b4d2
6yntar05 <6yntar05@mail.ru>
2024-02-01 20:47:23 +0700

    Prepare for db dev

---
+++ src/db/db.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <pqxx/pqxx>
+// Платформо-зависимые библиотеки лучше вынести в конкретную реализацию
+// см. https://github.com/Larinenok/BigEye
+// Только для теста!
+
+namespace db {
+
+void libraryTest() {
+    auto test = pqxx::connection();
+}
+
+} // namespace db
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <boost/asio/io_context.hpp>
 #include <websocketpp/config/asio.hpp>
 #include <websocketpp/server.hpp>
+#include "db/db.h"
 
 #include "CONAN_BUILD_TEST.h"
 
@@ -43,6 +44,7 @@ void set_tls_init_handler(Server& server) {
 int main() {
     // Просто тесты сборки
     turbowarp_cloud_cpp();
+    db::libraryTest();
 
     // Дальше приклюхи
     std::vector<std::string> vec;
